0df3fe09a39de67615cd8c715f751317faede3bb
z0ne <72435724+brayanzera@users.noreply.github.com>
2025-03-07 22:00:09 -0300

    Add files via upload

---
+++ trabalho_1BrayanSoares_07.03.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    int i, j, num, num_antes, cont;
+    
+    srand(time(NULL));
+    
+    int matriz[3][3], matriz_descon[3][3];
+	
+	/*= {
+		{0, 1 ,0},
+		{0, 1 ,0},
+		{0, 1, 1}
+	};*/
+    
+    for (i = 0; i < 3; i++) {
+    	for (j = 0; j < 3; j++) {
+          matriz[i][j] = rand() % 256;  
+        }
+   }
+
+    printf("MATRIZ\n");
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nRLE:\n");
+
+    num_antes = matriz[0][0];  
+    cont = 0;  
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) { 
+            num = matriz[i][j];
+			matriz_descon[i][j] = matriz[i][j];
+			
+            if (num == num_antes) {
+                cont++; 
+            } 
+			else {
+                printf("%d | %d  \n", cont, num_antes);
+                num_antes = num; 
+                cont = 1; 
+            }
+        }
+    }
+    printf("%d | %d\n", cont, num_antes);  
+    
+    printf("\n");
+    printf("MATRIZ DESCOMPACTADA\n");
+    
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", matriz_descon[i][j]);
+        }
+        printf("\n");
+    }
+}
+
